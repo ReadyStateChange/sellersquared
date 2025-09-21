2643939deb6d8bbf279ddf085c1afde70becbba3
Emmanuel Genard <5447389+edgenard@users.noreply.github.com>
2025-09-21 13:08:11 -0400

    Update storemapper posts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,7 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+import type { AstroSeoProps } from "@astrolib/seo";
+declare module "@astrolib/seo" {
+  export const AstroSeo: (props: AstroSeoProps) => any;
+}
